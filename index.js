fd12fe8c2cf9d31741db2298d4d291a318adc7ac
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2022-10-31 14:14:08 -0300

    FINAL COMMIT

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import app from "./src/server.js";
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT;
 
+
+//Iniciar app
 app.listen(PORT, () => {
 	console.log(`Discord passport backend online! Listening: ${PORT}.`);
 });
--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-undef */
 import cors from "cors";
-import express from "express";
+import express, { urlencoded } from "express";
 import path from "path";
 import "dotenv/config";
 import passport from "passport";
 import Strategy from "passport-discord";
+import { v4 as uuidv4 } from "uuid";
 
 import {fileURLToPath} from "url";
 import session from "express-session";
+import { renderFile } from "ejs";
 
 //Resolver __dirname undefined
 const __filename = fileURLToPath(import.meta.url);
@@ -18,12 +20,18 @@ const app = express();
 
 //Usando CORS para não ter error de politicas
 app.use(cors());
+app.use(urlencoded({ extended: true}));
 
-//session middleware
-
-app.use(session({ secret: "SECRET" })); // session secret
+//Session middleware
+app.use(session({ 
+	secret: uuidv4(),
+	resave: true,
+	saveUninitialized: true
+})); 
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+
+//Manter o login
+app.use(passport.session()); 
 
 //Definindo onde o express vai buscar os arquivos staticos para servir
 app.use(express.static(path.join(__dirname, "static")));
@@ -32,13 +40,26 @@ app.use(express.static(path.join(__dirname, "static")));
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "static", "index.html"));
 });
+
 app.get("/sucess", function (req, res) {
-	res.sendFile(path.join(__dirname, "static", "sucess.html"));
+	//Valida se existe sessão, se não houver ele redireciona para a primeira tela.
+	if(!req.session.passport){
+		res.redirect("/");
+	}else{
+		const user = req.session.passport.user;
+		renderFile(path.join(__dirname, "static", "sucess.ejs"), user, (err, html) => {
+			if(err){
+				return res.status(500).json(`${err}`);
+			}else{
+				return res.send(html);
+			}
+		});
+	}
 });
 
 
-//Configurar as permissões que ele vai buscar
-var scopes = ["identify", "email", "guilds", "guilds.join"];
+//Configurar as permissões que ele vai buscar na hora do login
+var scopes = ["identify", "email"];
 
 
 //API do passport
@@ -49,10 +70,11 @@ passport.use(new Strategy({
 	scope: scopes
 },
 function(accessToken, refreshToken, profile, cb) {
+	//Criando objeto da sessão para buscar esse objeto é so chamar "request.session".
 	const user = {
 		"ID": profile.id,
 		"USERNAME": profile.username,
-		"AVATAR": profile.avatar,
+		"AVATAR_URL": `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}`,
 		"EMAIL": profile.email,
 		"VERIFIED": profile.verified,
 		"PROVIDER": profile.provider,
@@ -63,19 +85,23 @@ function(accessToken, refreshToken, profile, cb) {
 	return cb(null, user);
 }));
 
-//
+//Salva a sessão
 passport.serializeUser(function(user, done) {
 	done(null, user);
 });
 
+//Remove a sessão
 passport.deserializeUser(function(user, done) {
 	done(null, user);
 });
 
 //Rotas da API
 app.get("/auth/discord", passport.authenticate("discord"));
+
+//Callback do discord
 app.get("/auth/discord/callback", passport.authenticate("discord", {
 	failureRedirect: "/",
 	successRedirect: "/sucess"
 }));
+
 export default app;
